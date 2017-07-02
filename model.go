74f6ecf8efc9be30a1f20661d565a6591269c30a
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2017-07-02 10:44:53 +0200

    sql queries

--- model.go
+++ model.go
@@ -2,31 +2,59 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type product struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
 
 func (p *product) getProduct(db *sql.DB) error {
-  return errors.New("Not implemented")
+	return db.QueryRow("SELECT name, price WHERE id=$1", p.ID).Scan(&p.Name, &p.Price)
 }
 
 func (p *product) updateProduct(db *sql.DB) error {
-  return errors.New("Not implemented")
+	_, err := db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3", p.Name, p.Price, p.ID)
+
+	return err
 }
 
 func (p *product) deleteProduct(db *sql.DB) error {
-  return errors.New("Not implemented")
+	_, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
+
+	return err
 }
 
 func (p *product) createProduct(db *sql.DB) error {
-  return errors.New("Not implemented")
+	err := db.QueryRow("INSERT INTO products(name, price) VALUES($1, $2) RETURNING id", p.Name, p.Price).Scan(&p.ID)
+
+	return err
 }
 
 func getProducts(db *sql.DB, start, count int) ([]product, error) {
-  return nil, errors.New("Not implemented")
+	rows, err := db.Query("SELECT id, name, price FROM products LIMIT $1 offset $2", count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Will execute at the end of the scope
+	defer rows.Close()
+
+	// products := []product{}
+	// https://github.com/golang/go/wiki/CodeReviewComments#declaring-empty-slices
+	var products []product
+
+	for rows.Next() {
+		var p product
+
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+
+		products = append(products, p)
+	}
+
+	return products, nil
 }
